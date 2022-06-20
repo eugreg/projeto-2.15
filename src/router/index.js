7430d7c1538bc0ec89c8a86a879a5c9bb15d79ae
Gui_T <you@example.com>
2022-06-20 09:19:26 -0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import CategoriaView from '../views/CategoriaView.vue'
+import AutorView from '../views/AutorView.vue'
+import LivrosView from '../views/LivrosView.vue'
+import EditorView from '../views/EditorView.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/Categoria',
+      name: 'Categorias',
+      component: CategoriaView
+     
+    },
+    {
+      path: '/autor',
+      name: 'Autores',
+      component: AutorView
+    },
+    {
+      patch: '/livros',
+      name: 'Livros',
+      component: LivrosView
+    },
+    {
+      path: '/editores',
+      name: 'Editores',
+      component: EditorView
+    },
+  ]
+})
+
+export default router
